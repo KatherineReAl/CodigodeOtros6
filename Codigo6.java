d8e759cbc3e507547c9433cb0f9555e05093a435
Katherine RA <kathephone1805@gmail.com>
2025-01-30 12:51:33 -0600

    se corrije y agrego comentarios al ejercicio de código de otros 6

--- Codigo6.java
+++ Codigo6.java
@@ -1,24 +1,35 @@
+package ejerciciocodigo6;
+/*
+ * El códigogenera un arreglo de 20 números aleatorios entre 20 y 400 y luego, solicita al usuario seleccionar una opción para resaltar los números que son múltiplos de 5 o 7. 
+ */
+import java.util.Scanner; //importo la biblioteca para usar el sacnner
 public class codigo6 {
-	  
-	    int[] n = int[20];
+	public static void main(String[] args) { //agregue el main y se ponen bien los corchetes para trabajar dentro de el main
+		 int[] n = new int[20]; //new
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
-	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
+		    for (int i = 0; i < 20; i++) {//i++
+		      n[i] = (int)(Math.random() * 381) + 20;
+		      System.out.print(n[i] + " ");//.out
+		    }
+		    
+		    System.out.println("\n¿Qué números quiere resaltar? ");//println
+		    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+		    
+		    Scanner scanner = new Scanner(System.in);
+	        int opcion = Integer.parseInt(scanner.nextLine()); //cambie las lineas para utilizar el scanner y así recibir la entrada del usuario
 
-	    int multiplo = (opcion == 1) : 5 ? 7;
+		    int multiplo = (opcion == 1) ? 5 : 7;//se cambia : 5 ? 7 por ? 5 : 7
 
-	    foreach (char e : n) {
-	      if (e % multiplo == 0) {
-	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
-	      }
-	    }
-	  
-	}
+		    //foreach (char e : n) {
+		    for (int i = 0; i < n.length; i++) {
+		      if (n[i] % multiplo == 0) { //cambie el foreach por un for y en el if quite e
+		        System.out.print("[" + n[i] + "] ");
+		       
+		      }else {
+		        System.out.print(n[i] + " ");//coriijo el print.in por out y cambio mi n[i]
+		      }
+		    }
+		
+} 
+  
+}
